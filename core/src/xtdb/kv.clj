0cc8f881db5a14313142259d8db29e580309f281
Jeremy Taylor <jdt@juxt.pro>
2022-05-29 17:28:08 +0100

    extend KvStore protocols with KvStoreTx and begin-kv-tx, add MergedKvIterator, implement these for MutableKvStore

--- core/src/xtdb/kv.clj
+++ core/src/xtdb/kv.clj
@@ -2,8 +2,10 @@
   "Protocols for KV backend implementations."
   (:refer-clojure :exclude [next])
   (:require [xtdb.io :as xio]
+            [xtdb.memory :as mem]
             [xtdb.status :as status]
-            [xtdb.system :as sys]))
+            [xtdb.system :as sys])
+  (:import java.io.Closeable))
 
 (defprotocol KvIterator
   (seek [this k])
@@ -15,6 +17,10 @@
   (new-iterator ^java.io.Closeable [this])
   (get-value [this k]))
 
+(defprotocol KvStoreTx
+  (abort-kv-store-tx [this])
+  (commit-kv-store-tx [this]))
+
 ;; tag::KvStore[]
 (defprotocol KvStore
   (new-snapshot ^java.io.Closeable [this])
@@ -23,7 +29,8 @@
   (compact [this])
   (count-keys [this])
   (db-dir [this])
-  (kv-name [this]))
+  (kv-name [this])
+  (begin-kv-tx [this]))
 ;; end::KvStore[]
 
 (def args
@@ -40,3 +47,97 @@
     {:xtdb.kv/kv-store (kv-name this)
      :xtdb.kv/estimate-num-keys (count-keys this)
      :xtdb.kv/size (some-> (db-dir this) (xio/folder-size))}))
+
+(deftype MergedKvIterator [db-iterator tx-db-iterator !db-iterated-last !last-seek-other-k]
+  KvIterator
+  (seek [_ k]
+    (let [db-k (seek db-iterator k)
+          tx-db-k (seek tx-db-iterator k)
+          cmp (.compare mem/buffer-comparator db-k tx-db-k)]
+      (cond
+        (neg? cmp) (do
+                     (reset! !db-iterated-last true)
+                     (reset! !last-seek-other-k tx-db-k)
+                     db-k)
+        (zero? cmp) (do
+                      (reset! !db-iterated-last false)
+                      (reset! !last-seek-other-k db-k)
+                      tx-db-k)
+        :else (do
+                (reset! !db-iterated-last false)
+                (reset! !last-seek-other-k db-k)
+                tx-db-k))))
+
+  (next [_]
+    (if @!db-iterated-last
+      (let [db-next-k (next db-iterator)
+            tx-db-k @!last-seek-other-k
+            cmp (.compare mem/buffer-comparator db-next-k tx-db-k)]
+        (cond
+          (neg? cmp) (do
+                       (reset! !db-iterated-last true)
+                       db-next-k)
+          (zero? cmp) (do
+                        (reset! !db-iterated-last false)
+                        (reset! !last-seek-other-k db-next-k)
+                        tx-db-k)
+          :else (do
+                  (reset! !db-iterated-last false)
+                  (reset! !last-seek-other-k db-next-k)
+                  tx-db-k)))
+      (let [tx-db-next-k (next tx-db-iterator)
+            db-k @!last-seek-other-k
+            cmp (.compare mem/buffer-comparator db-k tx-db-next-k)]
+        (cond
+          (neg? cmp) (do
+                       (reset! !db-iterated-last true)
+                       db-k)
+          (zero? cmp) (do
+                        (reset! !db-iterated-last false)
+                        (reset! !last-seek-other-k db-k)
+                        tx-db-next-k)
+          :else (do
+                  (reset! !db-iterated-last false)
+                  (reset! !last-seek-other-k db-k)
+                  tx-db-next-k)))))
+
+  (prev [_]
+    (if @!db-iterated-last
+      (let [db-prev-k (prev db-iterator)
+            tx-db-k @!last-seek-other-k
+            cmp (.compare mem/buffer-comparator db-prev-k tx-db-k)]
+        (cond
+          (neg? cmp) (do
+                       (reset! !db-iterated-last true)
+                       db-prev-k)
+          (zero? cmp) (do
+                        (reset! !db-iterated-last false)
+                        (reset! !last-seek-other-k db-prev-k)
+                        tx-db-k)
+          :else (do
+                  (reset! !db-iterated-last false)
+                  (reset! !last-seek-other-k db-prev-k)
+                  tx-db-k)))
+      (let [tx-db-prev-k (prev tx-db-iterator)
+            db-k @!last-seek-other-k
+            cmp (.compare mem/buffer-comparator db-k tx-db-prev-k)]
+        (cond
+          (neg? cmp) (do
+                       (reset! !db-iterated-last true)
+                       db-k)
+          (zero? cmp) (do
+                        (reset! !db-iterated-last false)
+                        (reset! !last-seek-other-k db-k)
+                        tx-db-prev-k)
+          :else (do
+                  (reset! !db-iterated-last false)
+                  (reset! !last-seek-other-k db-k)
+                  tx-db-prev-k)))))
+
+  (value [_]
+    (if @!db-iterated-last
+      (value db-iterator)
+      (value tx-db-iterator)))
+
+  Closeable
+  (close [_]))
--- core/src/xtdb/kv/mutable_kv.clj
+++ core/src/xtdb/kv/mutable_kv.clj
@@ -7,16 +7,16 @@
 
 (deftype MutableKvIterator [^NavigableMap db, !tail-seq]
   kv/KvIterator
-  (seek [this k]
+  (seek [_ k]
     (some-> (reset! !tail-seq (->> (.tailMap db (mem/as-buffer k) true)
                                    (filter val)))
             first
             key))
 
-  (next [this]
+  (next [_]
     (some-> (swap! !tail-seq rest) first key))
 
-  (prev [this]
+  (prev [_]
     (loop []
       (when-let [[[k _] :as tail-seq] (seq @!tail-seq)]
         (when-let [[k v :as lower-entry] (.lowerEntry db k)]
@@ -25,7 +25,7 @@
             k
             (recur))))))
 
-  (value [this]
+  (value [_]
     (some-> (first @!tail-seq) val))
 
   Closeable
@@ -33,8 +33,8 @@
 
 (deftype MutableKvSnapshot [^NavigableMap db]
   kv/KvSnapshot
-  (new-iterator [this] (->MutableKvIterator db (atom nil)))
-  (get-value [this k] (get db (mem/as-buffer k)))
+  (new-iterator [_] (->MutableKvIterator db (atom nil)))
+  (get-value [_ k] (get db (mem/as-buffer k)))
 
   ISeq
   (seq [_] (seq db))
@@ -42,21 +42,74 @@
   Closeable
   (close [_]))
 
+(deftype MutableKvTxSnapshot [db tx-db]
+  kv/KvSnapshot
+  (new-iterator [_]
+    (kv/->MergedKvIterator (->MutableKvIterator db (atom nil))
+                           (->MutableKvIterator tx-db (atom nil)) nil nil))
+  (get-value [_ k]
+    (or (kv/get-value tx-db (mem/as-buffer k))
+        (kv/get-value db (mem/as-buffer k))))
+
+  ISeq
+  (seq [_] (concat db tx-db))
+
+  Closeable
+  (close [_]))
+
+(declare ->mutable-kv-store)
+
+(defrecord MutableKvStoreTx [kv-store tx-kv-store]
+  kv/KvStore
+  (new-snapshot ^java.io.Closeable [_]
+    (->MutableKvTxSnapshot (kv/new-snapshot kv-store)
+                           (kv/new-snapshot tx-kv-store)))
+
+  (store [_ kvs] (kv/store tx-kv-store kvs))
+  (fsync [_])
+  (compact [_])
+  (count-keys [_] (+ (kv/count-keys kv-store) (kv/count-keys tx-kv-store)))
+  (db-dir [_])
+  (kv-name [this] (str (class this)))
+  (begin-kv-tx [this] (->MutableKvStoreTx this (->mutable-kv-store)))
+
+  kv/KvStoreTx
+  (abort-kv-store-tx [_])
+
+  (commit-kv-store-tx [_]
+    (when-let [s (seq (kv/new-snapshot tx-kv-store))]
+      (kv/store kv-store s))))
+
 (deftype MutableKvStore [^NavigableMap db]
   kv/KvStore
-  (new-snapshot ^java.io.Closeable [this]
+  (new-snapshot ^java.io.Closeable [_]
     (->MutableKvSnapshot db))
 
-  (store [this kvs]
+  (store [_ kvs]
     (doseq [[k v] kvs]
       (.put db (mem/as-buffer k) (some-> v mem/as-buffer))))
 
-  (fsync [this])
-  (compact [this])
-  (count-keys [this] (count db))
-  (db-dir [this])
-  (kv-name [this] (str (class this))))
+  (fsync [_])
+  (compact [_])
+  (count-keys [_] (count db))
+  (db-dir [_])
+  (kv-name [this] (str (class this)))
+  (begin-kv-tx [this] (->MutableKvStoreTx this (->mutable-kv-store))))
 
 (defn ->mutable-kv-store
   ([] (->mutable-kv-store nil))
   ([_] (->MutableKvStore (TreeMap. mem/buffer-comparator))))
+
+(comment
+  (let [m (->mutable-kv-store)
+        t (kv/begin-kv-tx m)
+        t2 (kv/begin-kv-tx t)
+        ]
+    (kv/store t2 [[(byte-array [(byte 1)]) (byte-array [(byte 4)])]])
+;;    (.getByte  (mem/as-buffer (kv/get-value (kv/new-snapshot t) (byte-array [(byte 1)]))) 0)
+
+    (kv/commit-kv-store-tx t2)
+    (kv/commit-kv-store-tx t)
+    (kv/get-value (kv/new-snapshot m) (byte-array [(byte 1)]))
+    )
+  )
