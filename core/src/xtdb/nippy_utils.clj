369bba6f52cee818236e6add445f538c1af3a168
Jeremy Taylor <jdt@juxt.pro>
2022-07-13 15:15:19 +0100

    doc->eid tweak

--- core/src/xtdb/nippy_utils.clj
+++ core/src/xtdb/nippy_utils.clj
@@ -28,12 +28,12 @@
   "Extract encoded eid from nippified document"
   [buf]
   (let [in (-> (DirectBufferInputStream. buf)
-               (DataInputStream.))]
+               (DataInputStream.))
+        b (edbb)]
     (assert (= id-map-sm (.readByte in)))
 
     (first (enc/reduce-n (fn [acc _]
-                           (let [b (edbb)
-                                 limited-b (nippyf/get-bytes-from-in! b 0 in)]
+                           (let [limited-b (nippyf/get-bytes-from-in! b 0 in)]
                              (if (mem/buffers=? idkb limited-b)
                                (do
                                  (conj acc (nippyf/get-bytes-from-in! b 0 in))) ;; TODO should break after match...don't really want a `reduce` here at all
